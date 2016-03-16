73b5f36f3ed1af3c518d294d41c9316d2e36a72c
dsanchez <dsanch17@uncc.edu>
2016-03-15 22:32:52 -0400

    t
    finalizing prototype, added title screens

---
+++ Assets/Scripts/returnToTitleScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class returnToTitleScript : MonoBehaviour {
+
+	public void returnToTitle()
+	{
+		SceneManager.LoadScene (3);
+	}
+}
---
+++ Assets/Scripts/titleScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class titleScript : MonoBehaviour {
+
+	public void playPrototype()
+	{
+		SceneManager.LoadScene (2);
+	}
+
+	public void viewInstructions()
+	{
+		SceneManager.LoadScene (4);
+	}
+}
