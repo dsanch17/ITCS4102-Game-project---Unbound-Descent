476f0372e3b2b9d27ad126d4512c8d9d2f15dd33
dsanchez <dsanch17@uncc.edu>
2016-04-01 00:07:43 -0400

    created a simple conveyor belt script

---
+++ Assets/Scripts/conveyorBeltScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class conveyorBeltScript : MonoBehaviour {
+
+	public float speed;
+	public bool goesCounterClockwise;
+
+
+	void Start () {
+		if (goesCounterClockwise)
+			speed = -speed;
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+
+		other.transform.Translate (new Vector3 (speed, 0, 0));
+	}
+}
